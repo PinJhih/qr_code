fc1dc438ced5a9396e33ed851d9f73ce4c912803
PinJhih <isaacwang29@gmail.com>
2020-06-25 10:09:55 +0800

    feat: 產生QR Code

--- app/src/main/java/com/example/qr_code/GenerateFragment.kt
+++ app/src/main/java/com/example/qr_code/GenerateFragment.kt
@@ -1,10 +1,16 @@
 package com.example.qr_code
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
+import kotlinx.android.synthetic.main.fragment_generate.*
 
 class GenerateFragment : Fragment() {
     override fun onCreateView(
@@ -12,4 +18,29 @@ class GenerateFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_generate, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_convert.setOnClickListener {
+            if(ed_input.text.isEmpty())
+                Toast.makeText(context, "請輸入文字", Toast.LENGTH_SHORT).show()
+            else
+                generateQRCode(ed_input.text.toString())
+        }
+    }
+
+    private fun generateQRCode(text: String) {
+        val writer = QRCodeWriter()
+        val bitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, 512, 512)
+        val width = bitMatrix.width
+        val height = bitMatrix.height
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                bitmap.setPixel(x, y, if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
+            }
+        }
+        img_qr_code.setImageBitmap(bitmap)
+    }
 }
